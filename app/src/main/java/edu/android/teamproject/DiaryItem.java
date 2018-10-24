0e5e0b381af5ac75b9466481220c5df78cf08fca
BanGwangHyeon <ilow123@naver.com>
2018-10-24 12:11:21 +0900

    DiaryItem class 생성

---
+++ app/src/main/java/edu/android/teamproject/DiaryItem.java
@@ -0,0 +1,60 @@
+package edu.android.teamproject;
+
+import java.util.Date;
+import java.util.List;
+
+public class DiaryItem {
+    private List<String> diaryImages;
+    private String diaryText;
+    private Date diaryDate;
+    private List<String> diaryTag;
+    private int diaryId;
+
+    public DiaryItem(List<String> diaryImages, String diaryText, Date diaryDate, List<String> diaryTag, int diaryId) {
+        this.diaryImages = diaryImages;
+        this.diaryText = diaryText;
+        this.diaryDate = diaryDate;
+        this.diaryTag = diaryTag;
+        this.diaryId = diaryId;
+    }
+
+    public List<String> getDiaryImages() {
+        return diaryImages;
+    }
+
+    public void setDiaryImages(List<String> diaryImages) {
+        this.diaryImages = diaryImages;
+    }
+
+    public String getDiaryText() {
+        return diaryText;
+    }
+
+    public void setDiaryText(String diaryText) {
+        this.diaryText = diaryText;
+    }
+
+    public Date getDiaryDate() {
+        return diaryDate;
+    }
+
+    public void setDiaryDate(Date diaryDate) {
+        this.diaryDate = diaryDate;
+    }
+
+    public List<String> getDiaryTag() {
+        return diaryTag;
+    }
+
+    public void setDiaryTag(List<String> diaryTag) {
+        this.diaryTag = diaryTag;
+    }
+
+    public int getDiaryId() {
+        return diaryId;
+    }
+
+    public void setDiaryId(int diaryId) {
+        this.diaryId = diaryId;
+    }
+}
