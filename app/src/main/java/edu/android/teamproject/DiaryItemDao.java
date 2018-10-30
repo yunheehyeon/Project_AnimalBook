94352edd624182719f98f995495e0d35c3851cd4
jaehyeok <dd>
2018-10-30 14:43:45 +0900

    로그인 파이어베이스 uid 설정 기능 추가 DiratyItemDao 생성 후 데이터베이스(firebase) 연결 완료

---
+++ app/src/main/java/edu/android/teamproject/DiaryItemDao.java
@@ -0,0 +1,97 @@
+package edu.android.teamproject;
+
+import android.net.Uri;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+
+public class DiaryItemDao implements ChildEventListener {
+
+    private FirebaseDatabase database;
+    private DatabaseReference reference;
+
+    private FirebaseAuth mAuth;
+    private String email;
+
+
+
+
+    private static DiaryItemDao diaryItemInstance;
+
+    public static DiaryItemDao getDiaryItemInstance(){
+        if(diaryItemInstance == null){
+            diaryItemInstance = new DiaryItemDao();
+        }
+
+        return diaryItemInstance;
+    }
+
+    private DiaryItemDao(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            for (UserInfo profile : user.getProviderData()) {
+                // Id of the provider (ex: google.com)
+                String providerId = profile.getProviderId();
+
+                // UID specific to the provider
+                String uid = profile.getUid();
+
+                // Name, email address, and profile photo Url
+                String name = profile.getDisplayName();
+                email =  profile.getEmail();
+                Uri photoUrl = profile.getPhotoUrl();
+            }
+
+        }
+
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference().child("DiaryItem");
+        reference.addChildEventListener(this);
+
+
+
+
+    }
+
+    public void insert(DiaryItem diaryItem){
+        diaryItem.setUid(email);
+        reference.push().setValue(diaryItem);
+
+
+    }
+
+
+    @Override
+    public void onChildAdded( DataSnapshot dataSnapshot,  String s) {
+
+    }
+
+    @Override
+    public void onChildChanged( DataSnapshot dataSnapshot,  String s) {
+
+    }
+
+    @Override
+    public void onChildRemoved( DataSnapshot dataSnapshot) {
+
+    }
+
+    @Override
+    public void onChildMoved( DataSnapshot dataSnapshot,  String s) {
+
+    }
+
+    @Override
+    public void onCancelled( DatabaseError databaseError) {
+
+    }
+}
