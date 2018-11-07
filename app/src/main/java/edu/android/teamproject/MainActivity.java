fdb4ba24160b7e9e643ea3b23b3c65f8f56014bf
BanGwangHyeon <ilow123@naver.com>
2018-11-07 09:54:48 +0900

    Action Bar 이벤트 추가

--- app/src/main/java/edu/android/teamproject/MainActivity.java
+++ app/src/main/java/edu/android/teamproject/MainActivity.java
@@ -10,6 +10,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -117,5 +119,12 @@ public class MainActivity extends AppCompatActivity{
         }
 
 
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.profile, menu);
+
+        return true;
     }
 }
